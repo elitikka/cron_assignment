42fad542fef2c164d9b8b13445593873fdf9313e
Elin <elin.tikkanen@gmail.com>
2025-11-25 18:53:33 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+import mysql.connector 
+import pandas as pd 
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path=".env")
+
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    database=os.getenv("DB_NAME")
+)
+df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50', 
+conn) 
+conn.close() 
+
+st.title('Säädata Helsingistä') 
+st.dataframe(df) 
